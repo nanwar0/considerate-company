8b837f524196e2c5f6df4b21fdeb065b5617f3b3
nanwar0 <nina.anwar@chicagobooth.edu>
2024-08-09 01:10:18 +0000

    added user accounts table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
 
+  root "home#index"
+  
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
